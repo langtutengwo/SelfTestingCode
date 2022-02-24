5df8574a98a98b1d7773aeb154f0d730b83aacad
langtutengwo <langtutengwo@gmail.com>
2022-02-24 16:55:18 -0500

    test

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -7,23 +7,25 @@ import org.junit.jupiter.api.Test;
 
 public class AppTest 
 {
-    @Test
+	@Test
+	
 	void testQ_rsqurtNormalInput() {
 		float a = 16;
-		float actual= findInverseSquareRoot.Q_rsqurt(a);
-        float expected = 0.25f;
-        float epsilon = Math.abs(actual - expected);
-        boolean result = false;
-        if (epsilon < 0.0001) {
-        	result = true;
-        }
-        assertTrue(result);
+		float expected = 0.25f;
+
+		float actual= FindInverseSquareRoot.q_rsqurt(a);
+		float epsilon = Math.abs(actual - expected);
+		boolean result = false;
+		if (epsilon < 0.0001) {
+			result = true;
+		}
+		assertTrue(result);
 	}
-	
+
 	@Test
 	void testQ_rsqurtInputIsZeroOrNegative() {
 		Exception exception = assertThrows(ArithmeticException.class, () -> {
-			findInverseSquareRoot.Q_rsqurt(-1);
+			FindInverseSquareRoot.q_rsqurt(-1);
 		});
 		String expectedMessage = "Input should be bigger than 0.";
 		String actualMessage = exception.getMessage();
