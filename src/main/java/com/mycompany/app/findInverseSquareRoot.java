e7f36c16d7061493a56e7d8c51ae8a312e87f178
langtutengwo <langtutengwo@gmail.com>
2022-03-02 17:04:34 -0500

    test

--- src/main/java/com/mycompany/app/findInverseSquareRoot.java
+++ src/main/java/com/mycompany/app/findInverseSquareRoot.java
@@ -1,26 +1,27 @@
 package com.mycompany.app;
 
-public class findInverseSquareRoot {
+public class FindInverseSquareRoot {
 
-		public static float Q_rsqurt(float x) {
-			if (x <= 0) {
-	        	throw new ArithmeticException("Input should be bigger than 0.");
-	        }
-	        int i;
-	        float x2, y;
-	        float thereehalfs = 1.5f;
-	
-	        x2 = x * 0.5f;
-	        y = x;
-	        i = Float.floatToIntBits(y);
-	        i = 0x5f3759df - (i >> 1);
-	        y = Float.intBitsToFloat(i);
-	        // 
-	        y = y * (thereehalfs - (x2 * y * y));
-	        y = y * (thereehalfs - (x2 * y * y));
-	        y = y * (thereehalfs - (x2 * y * y));
-	
-	        return y;
-	    }
+	public static float q_rsqurt(float x) {
+		if (x <= 0) {
+			throw new ArithmeticException("Input should be bigger than 0.");
+		}
+		int i;
+		float x2, y;
+		float threeHalfs = 1.5f;
+
+		x2 = x * 0.5f;
+		y = x;
+		i = Float.floatToIntBits(y);
+		// This subtraction can get a aproximate result. The 'magic number' is an approximation of square root of 2 ^ 127.
+		i = 0x5f3759df - (i >> 1);
+		y = Float.intBitsToFloat(i);
+		// More iteration, more precision.
+		y = y * (threeHalfs - (x2 * y * y));
+		y = y * (threeHalfs - (x2 * y * y));
+		y = y * (threeHalfs - (x2 * y * y));
+
+		return y;
+	}
 
 }
